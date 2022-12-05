1647052c7707051c5762e9ac9a8765db03ee86ea
MalingasCUMBANE <malingascumbane1@gmail.com>
2022-12-05 21:23:31 +0200

    asd

--- src/components/index.js
+++ src/components/index.js
@@ -3,7 +3,7 @@ export function Header() {
         <header className="header">
             <div className="container">
 
-                
+                <img src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" />
 
             </div>
         </header>
