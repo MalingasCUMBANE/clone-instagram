fd891695de03ddf749e4ebb52a1f55384bde89cd
MalingasCUMBANE <malingascumbane1@gmail.com>
2022-12-06 00:37:12 +0200

    asdas

---
+++ src/components/Layout/index.js
@@ -0,0 +1,16 @@
+export function Layout() {
+    return (
+        <>
+            <div className="box">
+                Storys
+            </div>
+            <div className="post">
+                Post
+            </div>
+            <div className="suggestionbox">
+                Sugestão
+            </div>
+
+        </>
+    )
+}
