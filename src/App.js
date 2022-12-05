a061c2dd5968ae90c57153eafef235fd8ac6750b
MalingasCUMBANE <malingascumbane1@gmail.com>
2022-12-06 00:46:25 +0200

    dsasd

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./styles/global.css"
 import React from 'react'
 import { Header } from "./components"
+import {Layout } from "./components/Layout"
 
 function App() {
   return (
     <div>
       <Header />
+      <Layout />
     </div>
   )
 }
