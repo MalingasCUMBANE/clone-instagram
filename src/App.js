f1fe9944337730fb4184ad36de34d04634ecaefa
MalingasCUMBANE <malingascumbane1@gmail.com>
2022-12-06 07:48:57 +0200

    das

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./styles/global.css"
 import React from 'react'
 import { Header } from "./components"
-import {Layout } from "./components/Layout"
+import { Layout } from "./components/Layout"
 
 function App() {
   return (
--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -3,14 +3,18 @@ import "./style.css"
 export function Layout() {
     return (
         <>
-            <div className="box">
-                Storys
-            </div>
-            <div className="post">
-                Post
-            </div>
-            <div className="suggestionbox">
-                Sugestão
+            <div className="MainGrid">
+                <div className="first-column" style={{ gridArea: "firstColumn" }}>
+                    <div className="box">
+                        Storys
+                    </div>
+                    <div className="post">
+                        Post
+                    </div>
+                </div>
+                <div className="suggestionbox" style={{ gridArea: "secondColumn" }}>
+                    Sugestão
+                </div>
             </div>
 
         </>
